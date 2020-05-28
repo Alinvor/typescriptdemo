a54029f6c452ba6716ca0ed352ff0a2c072250ea
dovsnier <dovsnier@qq.com>
2020-05-28 15:11:12 +0800

    向代码添加模块

--- src/greeter.ts
+++ src/greeter.ts
@@ -7,6 +7,10 @@ function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
+export function sayHello(name: string) {
+    return `Hello from ${name}`;
+}
+
 let user = { firstName: "Jane", lastName: "User" };
 
-document.body.innerHTML = greeter(user);
+// document.body.innerHTML = greeter(user);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { sayHello } from './greeter';
+
 function hello(compiler: string) {
     console.log(`Hello from ${compiler}`);
 }
 hello("TypeScript");
+console.log(sayHello("TypeScript with greeter.ts"));
